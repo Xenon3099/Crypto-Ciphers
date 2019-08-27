055c6d0bf9d3dc770531ec16099215e122faf352
Xenon3099 <dmamritesh@gmail.com>
2019-08-27 20:06:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from Additive import AddCipher
+from Multiplicative import Multicipher
+
+
 while True:
     print "Main Menu"
+    print "1. Additive Cipher"
+    print "2. Multiplicative Cipher"
+    print "3. Exit"
+    ch=input("Enter your choice: ")
+    if ch==1:
+        Additive()
+    elif ch==2:
+        Multiplicative()
+    elif ch==3:
+        break
+    print
+
+def Additive():
+    print "Additive Cipher Main Menu"
     print "1. Encrypt"
     print "2. Decrypt"
-    print "3. Exit"
+    print "3. Return"
     ch=input("Enter your choice: ")
     if ch==1:
         plain=raw_input("Enter the string to be encrypted : ")
@@ -21,4 +38,26 @@ while True:
         print "The decrypted string is : ",plain
     elif ch==3:
         break
-    print
+
+def Multiplicative():
+    print "Multiplicative Cipher Main Menu"
+    print "1. Encrypt"
+    print "2. Decrypt"
+    print "3. Return"
+    ch=input("Enter your choice: ")
+    if ch==1:
+        plain=raw_input("Enter the string to be encrypted : ")
+        key=input("Enter the Encryption key : ")
+        x=MultiCipher()
+        cipher=x.encrypt(plain,key)
+        print "Encrypted using ",x.name," Cipher"
+        print "The encrypted string is : ",cipher
+    elif ch==2:
+        cipher=raw_input("Enter the string to be decrypted : ")
+        key=input("Enter the Decryption key : ")
+        x=MultiCipher()
+        plain=x.decrypt(cipher,key)
+        print "Decrypted using ",x.name," Cipher"
+        print "The decrypted string is : ",plain
+    elif ch==3:
+        break
